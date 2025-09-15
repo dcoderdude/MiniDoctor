bdbc92bf79d5f279d0adc6b6b47abec2fb7cdafa
Dan Hernandez <daniel.hernandez.dev@gmail.com>
2025-09-14 18:05:08 -0700

    Play FixedSound on ObjectRect click
    - Added reference to FixedSound AudioStreamPlayer in ObjectRect
    - Updated click handler to play sound when ObjectRect is hidden
    - Used GetTree().CurrentScene to reliably access FixedSound

--- ObjectRect.cs
+++ ObjectRect.cs
@@ -3,9 +3,13 @@ using System;
 
 public partial class ObjectRect : ColorRect
 {
+	private AudioStreamPlayer _fixedSound;
+	
 	public override void _Ready()
 	{
 		GuiInput += OnGuiInput;
+		_fixedSound = GetTree().CurrentScene.GetNode<AudioStreamPlayer>("Audio/FixedSound");
+		GD.Print(_fixedSound != null ? "FixedSound found" : "FixedSound NOT found");
 	}
 
 	private void OnGuiInput(InputEvent @event)
@@ -13,6 +17,7 @@ public partial class ObjectRect : ColorRect
 		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
 		{
 			Hide();
+			_fixedSound.Play();
 		}
 	}
 }
